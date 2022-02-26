3a45d5fc2f6ad99991aaefeaeda0b4487f49f588
esfox <osairis100@gmail.com>
2022-02-26 11:37:38 +0800

    Setup SCSS and file-based routing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import 'virtual:windi.css';
+// vite-env.d.ts
+/// <reference types="vite-plugin-pages/client" />
 
-createApp(App).mount('#app');
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from '~pages';
+import App from './App.vue';
+
+import 'virtual:windi.css';
+import './styles/main.scss';
+
+const router = createRouter({ history: createWebHashHistory(), routes });
+
+createApp(App)
+  .use(router)
+  .mount('#app');
